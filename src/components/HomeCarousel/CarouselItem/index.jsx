d59d1f96265159e9091b10377e39913a649d8e27
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-10-10 10:22:40 -0300

    last update

--- src/components/HomeCarousel/CarouselItem/index.jsx
+++ src/components/HomeCarousel/CarouselItem/index.jsx
@@ -1,15 +1,15 @@
-export default function CarouselItem({ img, title, movieClass, released }) {
+export default function CarouselItem({ img, title, movieClass, released, banner }) {
     let year = released.split('-')[0]
     return (
         <div className={`${movieClass} `} data-bs-interval="10000">
 
-            <div className="card bg-dark text-white">
+            <div className="card bg-dark  text-white">
 
                 <img src={img} className="card-img"  alt="movie" />
-                <div className="card-img-overlay">
-                    <div className="overlayText">
-                        <h1>Peliss App</h1>
-                        <p>Un Lugar, todas las peliculas</p>
+                <div className="card-img-overlay d-flex align-items-center">
+                    <div className="overlayText text-center">
+                        <h1 className="mb-0">Peliss App</h1>
+                        <p >All your favorites {banner}</p>
                         <h1 className="card-title">{title}</h1>
                         <h3>{year}</h3>
                     </div>
--- src/components/HomeCarousel/index.jsx
+++ src/components/HomeCarousel/index.jsx
@@ -24,13 +24,13 @@ export default function HomeCarousel() {
 
     return (
         <div className="previewContainer" >
-            <div className="container-fluid py-2">
+            <div className="container py-2">
                 <div className="row justify-content-center">
-                    <div className="col-lg-4 text-center">Proxiammente Series</div>
-                    <div className="col-lg-4">
+                    
+                    <div className="col-lg-4 py-2 align-content-end">
                         {
                             popularMovies.length ?
-                                <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+                                <div id="carouselExampleInterval1" className="carousel slide" data-bs-ride="carousel">
                                     <div className="carousel-inner ">
                                         {
                                             popularMovies.map((movie, index) => {
@@ -44,6 +44,7 @@ export default function HomeCarousel() {
                                                     <CarouselItem
                                                         key={movie.id}
                                                         movieClass={movieClass}
+                                                        banner={"Documentals"}
                                                         img={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                                                         title={movie.original_title}
                                                         released={movie.release_date}
@@ -52,18 +53,92 @@ export default function HomeCarousel() {
                                             })
                                         }
                                     </div>
-                                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval1" data-bs-slide="prev">
                                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                                         <span className="visually-hidden">Previous</span>
                                     </button>
-                                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval1" data-bs-slide="next">
                                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                                         <span className="visually-hidden">Next</span>
                                     </button>
                                 </div> : null
                         }
                     </div>
-                    <div className="col-lg-4 text-center">Proximamente Novelas</div>
+                    <div className="col-lg-4 py-2 align-content-end">
+                        {
+                            popularMovies.length ?
+                                <div id="carouselExampleInterval2" className="carousel slide" data-bs-ride="carousel">
+                                    <div className="carousel-inner ">
+                                        {
+                                            popularMovies.map((movie, index) => {
+                                                let movieClass;
+                                                if (index === 0) {
+                                                    movieClass = "carousel-item active "
+                                                } else {
+                                                    movieClass = "carousel-item"
+                                                }
+                                                return (
+                                                    <CarouselItem
+                                                        key={movie.id}
+                                                        movieClass={movieClass}
+                                                        banner={"Series"}
+                                                        img={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                        title={movie.original_title}
+                                                        released={movie.release_date}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval2" data-bs-slide="prev">
+                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span className="visually-hidden">Previous</span>
+                                    </button>
+                                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval2" data-bs-slide="next">
+                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span className="visually-hidden">Next</span>
+                                    </button>
+                                </div> : null
+                        }
+                    </div>
+                    <div className="col-lg-4 py-2 align-content-end">
+                        {
+                            popularMovies.length ?
+                                <div id="carouselExampleInterval3" className="carousel slide" data-bs-ride="carousel">
+                                    <div className="carousel-inner ">
+                                        {
+                                            popularMovies.map((movie, index) => {
+                                                let movieClass;
+                                                if (index === 0) {
+                                                    movieClass = "carousel-item active "
+                                                } else {
+                                                    movieClass = "carousel-item"
+                                                }
+                                                return (
+                                                    <CarouselItem
+                                                        key={movie.id}
+                                                        movieClass={movieClass}
+                                                        banner={"Movies"}
+                                                        img={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                                        title={movie.original_title}
+                                                        released={movie.release_date}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval3" data-bs-slide="prev">
+                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span className="visually-hidden">Previous</span>
+                                    </button>
+                                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval3" data-bs-slide="next">
+                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span className="visually-hidden">Next</span>
+                                    </button>
+                                </div> : null
+                        }
+                    </div>
+                    
                 </div>
             </div>
         </div>
