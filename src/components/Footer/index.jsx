8fef4d011061dad1e68fbe8532cf3254304eef45
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-10-18 19:34:34 -0300

    Styles updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,13 +2,20 @@ import logoSofttek from '../../assets/images/softtek-white.webp'
 export default function Footer() {
     return (
         <footer className="footer text-center text-light">
-            <a href="https://www.softtek.com/es/" target={"_blank"} rel="noopener noreferrer">
-                <img src={logoSofttek} alt="logo" />
-            </a>
-            <p> Gabriel Vodopivec</p>
-            <a className='github' href="https://github.com/GabrielVodopivec" target="_blank" rel="noopener noreferrer">
-                github
-            </a>
+            <div className="wraper">
+                <a href="https://www.softtek.com/es/" target={"_blank"} rel="noopener noreferrer">
+                    <img src={logoSofttek} alt="logo" />
+                </a>
+            </div>
+            <div className="wraper">
+                <p> Gabriel Vodopivec</p>
+            </div>
+            <div className="wraper">
+                <a className='github' href="https://github.com/GabrielVodopivec" target="_blank" rel="noopener noreferrer">
+                    github
+                </a>
+            </div>
+
         </footer>
     )
 }
