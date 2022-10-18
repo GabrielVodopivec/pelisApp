409b97aa2625e20a3a0b71ef27964cac510dc6dd
GabrielVodopivec <vodopivecgabriel@gmail.com>
2022-10-18 18:40:12 -0300

    Styles updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,11 @@
+import logoSofttek from '../../assets/images/softtek-white.webp'
+
 export default function Footer() {
     return (
-        <footer className="footer text-center">
-            <p>Softtek - Gabriel Vodopivec</p>
+        <footer className="footer text-center text-light">
+            <img src={logoSofttek} alt="logo" />
+            
+            <p> Gabriel Vodopivec</p>
         </footer>
     )
 }
